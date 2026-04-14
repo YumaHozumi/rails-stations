f4e0f1600126dce1b3471546a1bf2ee7c5880ec9
YumaHozumi <y.hozumi@raksul.com>
2026-04-14 11:08:23 +0900

    feat: 編集機能を作成

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -24,6 +24,22 @@ class Admin::MoviesController < ApplicationController
         end
     end
 
+    def show
+        @movie = Movie.find(params[:id])
+    end
+
+    def edit
+        @movie = Movie.find(params[:id])
+    end 
+
+    def update
+        @movie = Movie.find(params[:id])
+        if @movie.update(movie_params)
+            redirect_to
+        
+        end
+    end
+
     private
         def movie_params
             params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url)
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :movies
-    post "movies" => "movies#create"
   end
 end
