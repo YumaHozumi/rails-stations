8c7684075ceaae8f82db2f880ce9890f17badd4d
YumaHozumi <y.hozumi@raksul.com>
2026-04-14 15:15:48 +0900

    feat: 絞り込み機能の実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,18 @@
 class MoviesController < ApplicationController
     def index
+        keyword = params[:keyword]
+        is_showing = params[:filter]
         @movies = Movie.all
+        
+        if keyword.present?
+            # 部分一致
+            @movies = Movie.where("name LIKE?", "%#{keyword}%").or(Movie.where("description LIKE?", "%#{keyword}%"))
+        end 
+
+        if is_showing == "showing"
+            @movies = @movies.where(is_showing: 1)
+        elsif is_showing == "scheduled"
+            @movies = @movies.where(is_showing: 0)
+        end
     end
 end
