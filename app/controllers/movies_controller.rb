d0a69f438b5d34dc377c3ecbe2777f571da51e7f
YumaHozumi <y.hozumi@raksul.com>
2026-04-21 16:52:07 +0900

    perf: パフォーマンスを改善

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,17 +2,16 @@ class MoviesController < ApplicationController
   def index
     keyword = params[:keyword]
     is_showing = params[:is_showing]
-    @movies = Movie.all
 
-    if keyword.present?
+    @movies = if keyword.present?
       # 部分一致
-      @movies = @movies.where('name LIKE?', "%#{keyword}%").or(Movie.where('description LIKE?', "%#{keyword}%"))
-    end
-
-    if is_showing == 'showing'
-      @movies = @movies.where(is_showing: 1)
+      Movie.where('name LIKE?', "%#{keyword}%").or(Movie.where('description LIKE?', "%#{keyword}%"))
+    elsif is_showing == 'showing'
+        Movie.where(is_showing: 1)
     elsif is_showing == 'scheduled'
-      @movies = @movies.where(is_showing: 0)
+       Movie.where(is_showing: 0)
+    else
+        Movie.all
     end
   end
 
